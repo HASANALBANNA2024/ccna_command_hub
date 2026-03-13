79e079aae4f8dd53283a3f970c84cac52bb8fd4e
HASANALBANNA2024 <albannamdhasan48@gmail.com>
2026-03-14 04:33:43 +0600

    database setup ok but not code ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:ccna_command_hub/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ccna_command_hub/themes/app_theme.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 // value notifier for Dark Mode
 final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
 
-void main() {
+void main() async {
+  // এটি অবশ্যই সবার আগে দিতে হবে
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ফায়ারবেস ইনিশিয়ালাইজ করা
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
